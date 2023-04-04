5657b6c821c1e172d7b03efacaceffaa68dd1c6f
PhucChiVas161 <vuatrochoi159@gmail.com>
2023-04-04 23:30:13 +0700

    Update code Notification

--- src/Views/App/App.js
+++ src/Views/App/App.js
@@ -1,17 +1,17 @@
-import '../App/App.css';
-import Footer from '../Footer/Footer';
-import Home from '../Home/Home';
-import Mastheader from '../Mastheader/Mastheader';
-import Nav from '../Nav/Nav';
+import "../App/App.css";
+import Footer from "../Footer/Footer";
+import Home from "../Home/Home";
+import Mastheader from "../Mastheader/Mastheader";
+import Nav from "../Nav/Nav";
+
 function App() {
   return (
-        <div id="page-top" >
-          <Nav></Nav>
-          <Mastheader></Mastheader>
-          <Home></Home>
-          <Footer></Footer>
-        </div>
-
+    <div id="page-top">
+      <Nav></Nav>
+      <Mastheader></Mastheader>
+      <Home></Home>
+      <Footer></Footer>
+    </div>
   );
 }
 
--- src/Views/Home/NofiCation/Nofication.js
+++ src/Views/Home/NofiCation/Nofication.js
@@ -1,37 +1,81 @@
-import '../NofiCation/css/style.css';
-import { AiFillBell } from "react-icons/ai";
+import "../NofiCation/css/style.css";
+import { useState, useEffect } from "react";
+import { Container, Row, Col, ListGroup, Card } from "react-bootstrap";
 function Notification() {
-  return ( 
+  const [posts, setPosts] = useState([]);
+  const [selectedPost, setSelectedPost] = useState(null);
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/PhucChiVas161/FE-Intern/News")
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.error(error));
+  }, []);
+  const handlePostClick = (post) => {
+    if (selectedPost && selectedPost.id === post.id) {
+      setSelectedPost(null);
+    } else {
+      setSelectedPost(post);
+    }
+  };
+  return (
     <section id="faq" className="faq section-bg">
-            <div className="container">
-              <div className="section-title wow animated fadeInUp animated" style={{visibility: 'visible', animationName: 'fadeInUp'}}>
-                <p  data-aos="fade-up">THÔNG BÁO</p>
-                <h2  data-aos="fade-up" >.</h2>
-              </div>
-              <div className="faq-list">
-                <ul>
-                  <li className="mb-3 fadeInLeft wow animated animated  animated" data-wow-duration="1s" style={{visibility: 'visible', animationDuration: '1s', animationName: 'fadeInLeft'}}>
-                    <AiFillBell className="bx bx-help-circle icon-help"> </AiFillBell> 
-                    <a style={{textDecoration:'none'}} data-toggle="collapse" className="collapse" href="#faq-list-1" aria-expanded="false" aria-controls="collapseOne">Nguyên tắc chung sinh viên cần thực hiện để giải quyết một công việc là gì?
-                    <i className="bx bx-chevron-down icon-close"></i><i className="bx bx-chevron-up icon-show"></i></a>
-                    <div id="faq-list-1" className="collapse p-3" data-bs-parent=".faq-list">
-
-                      <p>
-                      </p><p>Khi cần giải quyết một công việc gì liên quan đến bản thân mình, sinh viên phải thực hiện theo nguyên tắc sau đây:</p>
-                      <p>-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Tìm hiểu và nắm rõ về công việc cần giải quyết, thông qua các tài liệu do Trường ban hành và công bố tại website. Các thông tin cần phải nắm rõ tối thiểu gồm: các bước tiến hành, loại đơn/biểu mẫu cần sử dụng, nơi nhận/trả hồ sơ, thời gian dự kiến trả hồ sơ;</p>
-                      <p>-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Chuẩn bị đúng và đủ hồ sơ;</p>
-                      <p>-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Đến liên hệ đúng nơi nhận hồ sơ và đúng thời gian quy định nhận hồ sơ để giải quyết công việc;</p>
-                      <p>-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Trang phục, tác phong nghiêm túc; trình thẻ sinh viên và hồ sơ cho bộ phận tiếp nhận; nhận lại giấy hẹn trả hồ sơ; đóng lệ phí đối với những trường hợp có thu lệ phí.</p>
-                      <p />
-                    </div>
-                  </li>
-
-                </ul>
-              </div>
-            </div>
+      <div className="container">
+        <div
+          className="section-title wow animated fadeInUp animated"
+          style={{ visibility: "visible", animationName: "fadeInUp" }}
+        >
+          <p data-aos="fade-up">THÔNG BÁO</p>
+          <h2 data-aos="fade-up">.</h2>
+        </div>
+        <div className="faq-list">
+          <Container className="py-3">
+            <Row>
+              <Col md={12}>
+                <ListGroup>
+                  {posts.map((post) => (
+                    <ListGroup.Item
+                      key={post.id}
+                      action
+                      onClick={() => handlePostClick(post)}
+                      active={selectedPost && selectedPost.id === post.id}
+                      style={{
+                        display: "block",
+                        marginBottom: "1rem",
+                        textAlign: "center",
+                      }}
+                    >
+                      {post.title_content}
+                      {selectedPost && selectedPost.id === post.id && (
+                        <Card>
+                          <Card.Body>
+                            <Card.Title
+                              style={{ color: "#000", textAlign: "left" }}
+                            >
+                              {selectedPost.title_content}
+                            </Card.Title>
+                            <Card.Text
+                              style={{ color: "#000", textAlign: "left" }}
+                            >
+                              {selectedPost.content}
+                            </Card.Text>
+                          </Card.Body>
+                          <Card.Img
+                            className="img-fluid"
+                            variant="top"
+                            src={selectedPost.img}
+                          />
+                        </Card>
+                      )}
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
     </section>
-          
-   );
+  );
 }
 
 export default Notification;
