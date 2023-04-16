ab2e3e45b5272a8e9e3d9fac3b823b473ef59cd1
Your Name <you@example.com>
2023-04-16 20:12:40 +0700

    add project signup

--- src/pages/LandingPage/Content/Content.js
+++ src/pages/LandingPage/Content/Content.js
@@ -1,9 +1,13 @@
 import Notification from "./Notification/Notification";
+import Project from "./Project/Project";
+import Signup from "./Signup/Signup";
 
 function Content() {
     return ( 
         <>
             <Notification />
+            <Project />
+            <Signup />
 
         </>
      );
