19c24632d6daa8725f49367f2c74164c06fd3f65
PhucChiVas161 <vuatrochoi159@gmail.com>
2024-03-21 10:45:13 +0700

    Update: PostponeManagement

--- src/pages/PostponeExamPage/PostponeManagement.jsx
+++ src/pages/PostponeExamPage/PostponeManagement.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
-import { DataGridPremium } from '@mui/x-data-grid-premium';
+import { DataGridPremium, useKeepGroupedColumnsHidden, useGridApiRef } from '@mui/x-data-grid-premium';
 import { getPostponeExamAll } from './PostponeExamAPI';
 import { Helmet } from 'react-helmet';
 import { LinearProgress } from '@mui/material';
+import Header from '../../components/Header';
 
 const PostponeManagement = () => {
   const [postponeExam, setPostponeExam] = useState([]);
@@ -29,45 +30,62 @@ const PostponeManagement = () => {
   const columns = [
     { field: 'id', headerName: 'STT', flex: 0.5 },
     {
-      field: 'employeeName',
-      headerName: 'Họ và tên',
-      flex: 1,
-      cellClassName: 'name-column--cell',
-    },
-    {
-      field: 'accountEmail',
-      headerName: 'Email',
+      field: 'tenHP',
+      headerName: 'Tên học phần',
       flex: 1,
     },
     {
-      field: 'employeeMSSV',
-      headerName: 'MSSV',
+      field: 'lanThi',
+      headerName: 'Lần thi',
       flex: 1,
     },
     {
-      field: 'postponeExamCode',
+      field: 'lyDo',
+      headerName: 'Lý do',
+      flex: 1,
+    },
+    {
+      field: 'postponeExamId',
       headerName: 'Mã đơn',
       flex: 1,
     },
-
     {
-      field: 'createAt',
-      headerName: 'Ngày tạo',
+      field: 'status',
+      headerName: 'Trạng thái',
+      flex: 1,
+    },
+    {
+      field: 'phanHoi',
+      headerName: 'Phản hồi',
+      flex: 1,
+    },
+    {
+      field: 'ghiChu',
+      headerName: 'Ghi chú',
       flex: 1,
     },
     {
       field: 'actions',
       headerName: 'Hành động',
-      flex: 0.5,
       disableExport: true,
+      flex: 0.5,
     },
   ];
-
+  const apiRef = useGridApiRef();
+  const initialState = useKeepGroupedColumnsHidden({
+    apiRef,
+    initialState: {
+      rowGrouping: {
+        model: ['status'],
+      },
+    },
+  });
   return (
     <>
       <Helmet>
         <title>Quản lý HOÃN THI | KHẢO THÍ - VLU</title>
       </Helmet>
+      <Header title="Quản lý HOÃN THI" />
       <DataGridPremium
         slots={{
           loadingOverlay: LinearProgress,
@@ -75,6 +93,9 @@ const PostponeManagement = () => {
         loading={loading}
         rows={transformedPostponeExam}
         columns={columns}
+        getRowHeight={() => 'auto'}
+        initialState={initialState}
+        apiRef={apiRef}
       />
     </>
   );
